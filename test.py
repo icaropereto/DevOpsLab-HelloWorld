ed3c5c26e4917ae0a1ebcfc496228712f0dcfa55
icaro_pereto <icaro_pereto@hotmail.com>
2019-09-15 12:38:35 -0400

    update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Icaro Pereto"")
+        self.assertRegex(result.data.decode(), "Icaro Pereto.")
 
 
 if __name__ == "__main__":
